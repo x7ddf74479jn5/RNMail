c5dfd595e3c5123e6f254bb72d7ee347fd3d2129
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-07-19 21:13:49 +0900

    ✨ add: notebook list to the bottom sheet

--- src/components/move-note-sheet.tsx
+++ src/components/move-note-sheet.tsx
@@ -1,7 +1,14 @@
-import React, {forwardRef, useImperativeHandle, useMemo, useRef} from "react"
+import React, {
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+} from "react"
 import RNBottomSheet, {BottomSheetBackdrop} from "@gorhom/bottom-sheet"
 import BottomSheet from "@/components/bottom-seat"
 import {Box, Text} from "@/atoms"
+import BookList from "@/components/book-list"
 
 interface MoveNoteSheetProps {
   onClose?: () => void
@@ -25,6 +32,13 @@ const MoveNoteSheet = forwardRef<MoveNoteSheetHandle, MoveNoteSheetProps>(
       },
     }))
 
+    const handlePressItem = useCallback((bookId: string) => {
+      const {current: bottomSheet} = refBottomSheet
+      if (bottomSheet) {
+        bottomSheet.close()
+      }
+    }, [])
+
     return (
       <BottomSheet
         ref={refBottomSheet}
@@ -44,9 +58,16 @@ const MoveNoteSheet = forwardRef<MoveNoteSheetHandle, MoveNoteSheetProps>(
           marginHorizontal: 12,
         }}
         onClose={onClose}>
-        <Box justifyContent="center" alignItems="center">
-          <Text fontWeight="bold">Move</Text>
-        </Box>
+        <BookList
+          inBottomSheet
+          onPressItem={handlePressItem}
+          color="$foreground"
+          headerComponent={() => (
+            <Box justifyContent="center" alignItems="center">
+              <Text fontWeight="bold">Move</Text>
+            </Box>
+          )}
+        />
       </BottomSheet>
     )
   },
